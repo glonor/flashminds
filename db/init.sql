3e7139e5ccb26fd25a27845424b4959399f1c920
pietrolechthaler <pietrolechthaler@gmail.com>
2023-11-20 09:50:28 +0100

    updated database scheme

--- db/init.sql
+++ db/init.sql
@@ -2,16 +2,27 @@
 CREATE DATABASE IF NOT EXISTS flashcard_db;
 USE flashcard_db;
 
--- Create a table to store flashcards
-CREATE TABLE IF NOT EXISTS flashcards (
-    card_id INT AUTO_INCREMENT PRIMARY KEY,
-    front_content VARCHAR(255) NOT NULL,
-    back_content VARCHAR(255) NOT NULL,
+-- Create a table to store users
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT PRIMARY KEY,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create a table to store flashcards
+CREATE TABLE IF NOT EXISTS flashcards (
+    card_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    front_content VARCHAR(255) NOT NULL,
+    back_content VARCHAR(255) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
 -- Sample data (you can customize or remove this part)
+
+INSERT INTO users (user_id) VALUES (1), (2), (3);
+
 INSERT INTO flashcards (front_content, back_content) VALUES
-    ('Capital of France', 'Paris'),
-    ('Largest mammal', 'Blue Whale'),
-    ('Programming language for web development', 'JavaScript');
+    (1, 'Capital of France', 'Paris'),
+    (2, 'Largest mammal', 'Blue Whale'),
+    (3, 'Programming language for web development', 'JavaScript');
