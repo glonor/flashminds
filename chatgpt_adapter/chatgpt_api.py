8f308a6b9a931956c121a43fb7b19238944f1bc6
glonor <mouezkhelifi96@gmail.com>
2023-11-30 15:09:51 +0100

    Added multiple attempts to chatgpt api call

--- chatgpt_adapter/chatgpt_api.py
+++ chatgpt_adapter/chatgpt_api.py
@@ -7,26 +7,38 @@ app = Flask(__name__)
 
 client = OpenAI(api_key=os.environ.get('OPENAI_API_KEY'))
 
+MAX_RETRIES = 3
+
 @app.route('/paraphrase', methods=['GET'])
 def paraphrase():
     try:
         # Extract the input flashcard from the request
         input_flashcard = request.json.get('flashcard')
 
-        # Use OpenAI function to generate paraphrased flashcard
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "As a paraphraser, generate a variation of json formatted flashcards that are similar to the original flashcards"},
-                {"role": "user", "content": input_flashcard},
-            ]
-        )
 
-        # Extract the paraphrased flashcard from OpenAI response
-        paraphrased_flashcard = json.loads(completion.choices[0].message.content)
+        for attempt in range(MAX_RETRIES):
+            try:
+                # Use OpenAI function to generate paraphrased flashcard
+                completion = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "As a paraphraser, generate a variation of json formatted flashcards that are similar to the original flashcards"},
+                        {"role": "user", "content": input_flashcard},
+                    ]
+                )
 
-        # Return the paraphrased flashcard as JSON response
-        return jsonify(paraphrased_flashcard)
+                # Extract the paraphrased flashcard from OpenAI response
+                paraphrased_flashcard = json.loads(completion.choices[0].message.content)
+
+                # Return the paraphrased flashcard as JSON response
+                return jsonify(paraphrased_flashcard), 200
+
+            except json.JSONDecodeError:
+                # Retry if JSON loading fails
+                if attempt < MAX_RETRIES - 1:
+                    continue
+                else:
+                    return jsonify({'message': 'Unable to generate a valid JSON representation from ChatGPT after multiple attempts'}), 500
 
     except Exception as e:
         # Handle errors and return an appropriate response
@@ -38,20 +50,30 @@ def generate_flashcard():
         # Extract the input text from the request
         input_text = request.json.get('text')
 
-        # Use OpenAI function to generate a flashcard as per the system message
-        completion = client.chat.completions.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "system", "content": "As a flashcard creator, generate RFC8259 compliant JSON responses adhering strictly to the specified format, including only the question and answer without any additional explanations"},
-                {"role": "user", "content": input_text},
-            ]
-        )
+        for attempt in range(MAX_RETRIES):
+            try:
 
-        # Extract the generated flashcard from OpenAI response
-        generated_flashcard = json.loads(completion.choices[0].message.content)
+                # Use OpenAI function to generate a flashcard as per the system message
+                completion = client.chat.completions.create(
+                    model="gpt-3.5-turbo",
+                    messages=[
+                        {"role": "system", "content": "As a flashcard creator, generate RFC8259 compliant JSON responses adhering strictly to the specified format, including only the question and answer without any additional explanations"},
+                        {"role": "user", "content": input_text},
+                    ]
+                )
 
-        # Return the generated flashcard as JSON response
-        return jsonify(generated_flashcard)
+                # Extract the generated flashcard from OpenAI response
+                generated_flashcard = json.loads(completion.choices[0].message.content)
+
+                # Return the generated flashcard as JSON response
+                return jsonify(generated_flashcard), 200
+            
+            except json.JSONDecodeError:
+                    # Retry if JSON loading fails
+                    if attempt < MAX_RETRIES - 1:
+                        continue
+                    else:
+                        return jsonify({'message': 'Unable to generate a valid JSON representation from ChatGPT after multiple attempts'}), 500
 
     except Exception as e:
         # Handle errors and return an appropriate response
