28b4c30b4fe3f42b8094dc9c474630c8061fa7f6
glonor <mouezkhelifi96@gmail.com>
2023-12-01 11:03:15 +0100

    Reverted

--- tests/test_scheduler.py
+++ tests/test_scheduler.py
@@ -183,3 +183,100 @@ def test_get_next_flashcard_with_expired_session():
     delete_user_url = f'{DL_API_URL}/users/{user_id}'
     delete_user_response = requests.delete(delete_user_url)
     assert delete_user_response.status_code == 200
+
+def test_start_study_session():
+    # Create a user and a deck
+    _, deck_id, user_id = create_test_flashcard()
+
+    # Start a study session
+    start_session_url = f'{base_url}/start_study_session'
+    start_session_payload = {
+        "user_id": user_id,
+        "deck_id": deck_id
+    }
+    start_session_response = requests.post(start_session_url, json=start_session_payload)
+
+    # Check the response from the server
+    assert start_session_response.status_code == 201
+    assert 'message' in start_session_response.json()
+    assert 'session_id' in start_session_response.json()
+
+    # Clean up: Delete the user and associated data
+    delete_user_url = f'{DL_API_URL}/users/{user_id}'
+    delete_user_response = requests.delete(delete_user_url)
+    assert delete_user_response.status_code == 200
+
+def test_end_study_session():
+    # Create a user, a deck, and start a study session
+    flashcard_id, deck_id, user_id = create_test_flashcard()
+    start_session_url = f'{base_url}/start_study_session'
+    start_session_payload = {
+        "user_id": user_id,
+        "deck_id": deck_id
+    }
+    start_session_response = requests.post(start_session_url, json=start_session_payload)
+    assert start_session_response.status_code == 201
+    session_id = start_session_response.json()['session_id']
+
+    # Review the flashcard
+    review_flashcard_url = f'{base_url}/review_flashcard'
+    review_payload = {
+        "user_id": user_id,
+        "deck_id": deck_id,
+        "card_id": flashcard_id,
+        "confidence": 5,
+        "session_id": session_id
+    }
+    review_flashcard_response = requests.post(review_flashcard_url, json=review_payload)
+    assert review_flashcard_response.status_code == 200
+
+    # End the study session
+    end_session_url = f'{base_url}/end_study_session'
+    end_session_payload = {
+        "user_id": user_id,
+        "deck_id": deck_id,
+        "session_id": session_id
+    }
+    end_session_response = requests.post(end_session_url, json=end_session_payload)
+
+    # Check the response from the server
+    assert end_session_response.status_code == 200
+    assert 'message' in end_session_response.json()
+    assert 'average_confidence' in end_session_response.json()
+
+    # Clean up: Delete the user and associated data
+    delete_user_url = f'{DL_API_URL}/users/{user_id}'
+    delete_user_response = requests.delete(delete_user_url)
+    assert delete_user_response.status_code == 200
+
+def test_start_study_session_after_session_over():
+    # Create a user, a deck, and start a study session
+    _, deck_id, user_id = create_test_flashcard()
+    start_session_url = f'{base_url}/start_study_session'
+    start_session_payload = {
+        "user_id": user_id,
+        "deck_id": deck_id
+    }
+    start_session_response = requests.post(start_session_url, json=start_session_payload)
+    assert start_session_response.status_code == 201
+    session_id = start_session_response.json()['session_id']
+
+    # End the study session
+    end_session_url = f'{base_url}/end_study_session'
+    end_session_payload = {
+        "user_id": user_id,
+        "deck_id": deck_id,
+        "session_id": session_id
+    }
+    end_session_response = requests.post(end_session_url, json=end_session_payload)
+    assert end_session_response.status_code == 200
+
+    # Attempt to end the study session again
+    end_session_response = requests.post(end_session_url, json=end_session_payload)
+    assert end_session_response.status_code == 400
+
+    # Clean up: Delete the user and associated data
+    delete_user_url = f'{DL_API_URL}/users/{user_id}'
+    delete_user_response = requests.delete(delete_user_url)
+    assert delete_user_response.status_code == 200
+
